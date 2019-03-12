aee96bed752204e0f25fc3860f99971102c5f8d7
ocean010101 <yangyang01.xu@outlook.com>
2019-03-12 20:38:43 +0800

    Answer
    Signed-off-by: ocean010101 <yangyang01.xu@outlook.com>

--- test/test.js
+++ test/test.js
@@ -1,9 +1,8 @@
-var assert = require('assert')
-
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1, 2, 3]/* 填空题 */)
+var assert = require('assert');
+describe('Array', function () {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal(-1, [1, 2, 3].indexOf(4));
     })
   })
 })
@@ -14,14 +13,14 @@ describe('assert', function () {
       c: {
         e: 1
       }
-    }
+    };
     var b = {
       c: {
         e: 1
       }
-    }
+    };
     // 修改下面代码使得满足测试描述
-    assert.equal(a, b)
+    assert.deepStrictEqual(a, b);
   })
 
   it('可以捕获并验证函数fn的错误', function () {
@@ -29,6 +28,6 @@ describe('assert', function () {
       xxx;
     }
     // 修改下面代码使得满足测试描述
-    fn()
+    assert.throws(fn);
   })
 })
